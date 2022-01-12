9d7babdf451a298c7b471448e09bde8236b96a91
Mirte Houwing <mirtehouwing@Mirtes-MacBook-Pro.local>
2022-01-12 15:01:36 +0100

    Homework Done

--- loops.js
+++ loops.js
@@ -6,7 +6,10 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log('Hoera!');
+}
+console.log("\n")
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -18,8 +21,22 @@
 // klaar!
  // ==========================================
 
+// for (let j = 0; j < 5; j++) {
+//     if (j === 4) {
+//         console.log("Klaar!");
+//     } else {
+//         console.log("loop...");
+//     }
+// }
 
-
+for (let j = 0; j < 5; j++) {
+    if (j <= 3) {
+        console.log("Loop...");
+    } else {
+        console.log("Klaar");
+    }
+}
+console.log("\n")
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
@@ -32,8 +49,14 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
-
-
+for (let k = 0; k < 16; k++) {
+    if (k % 2 === 0) {
+        console.log(k + ' is even');
+    } else {
+        console.log(k + ' is oneven')
+    }
+}
+console.log("\n")
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -50,4 +73,12 @@
 // >> 8
 // >> 9
 // ==========================================
-
+for (let l = 0; l < 10; l++) {
+    if (l > 2 && l < 6) {
+        console.log('> ' + l);
+    } else if (l > 5 && l < 10) {
+        console.log('>> ' + l);
+    } else {
+        console.log('' + l);
+    }
+}
